ccc0cd5b838b748d12b0db314c75dbf9bb9462b0
Highfloor <k01096789705@gmail.com>
2023-03-24 13:52:10 +0900

    feat: d1.py 로그인과정 구현

--- basic/d1.py
+++ basic/d1.py
@@ -54,6 +54,11 @@
         ENGINE=InnoDB
         ;
 
+        # 컬럼추가
+        # 컬럼변경
+        # 컬럼 이름 포함 번경
+        # 컬럼 삭제
+
         ALTER TABLE `users`
         CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '고객 고유 관리 ID' FIRST,
         CHANGE COLUMN `uid` `uid` VARCHAR(32) NOT NULL COMMENT '고객의 로그인 ID' COLLATE 'utf8mb4_general_ci' AFTER `id`,
@@ -61,5 +66,76 @@
         CHANGE COLUMN `name` `name` VARCHAR(30) NOT NULL COMMENT '고객의 이름' COLLATE 'utf8mb4_general_ci' AFTER `upw`,
         CHANGE COLUMN `regdate` `regdate` TIMESTAMP NOT NULL COMMENT '고객의 가입일' AFTER `name`;
         SELECT `DEFAULT_COLLATION_NAME` FROM `information_schema`.`SCHEMATA` WHERE `SCHEMA_NAME`='ml_db';
+    
+
+    6. 회원 가입 -> insert
+        - insert into [<데이터베이스명>,]<테이블명>
+            [(컬럼명,컬럼명,컬럼명,...)]
+         values
+            ('값','값','값',...);
+
+        - 키워드를 테이블명, 컬럼명등등 사용하면 백틱( ` )을 통해 처리가능
+        - '값'
+        - now():mysql의 내장함수로서,현재시간을 리턴
+        
+        - INSERT INTO 
+            `ml_db`.`users` 
+            (`uid`, `upw`, `name`, `regdate`) 
+         VALUES 
+            ('guest', '1234', '게스트', now());
+
+    # 실행 결과
+        # 회원가입 쿼리
+        -- 회원가입 쿼리-> 영향받은 행의 수를 체크
+        INSERT INTO 
+            `ml_db`.`users` 
+            (`uid`, `upw`, `name`, `regdate`) 
+         VALUES 
+            ('guest2', '1234', '게스트', now());
+        /* 영향 받은 행: 1  찾은 행: 0  경고: 0  지속 시간 1 쿼리: 0.016 초 */
+
+    7. 로그인 -> select (쿼리의 분량이 가장 많다)
+        -- 회원 가입 여부 조회 -> 로그인 처리 쿼리
+        -- 대소문자 구분 안한다
+        -- 제시한 아이디와 비번과 일치하는 row데이터를
+        -- 가져와서 uid,name,regdate값을 출력하시오
+        SELECT
+            uid,`NAME`
+        FROM
+            users
+        WHERE
+            uid='guest'
+        AND
+            upw='1234';
+
+    8. 회원 정보 수정 -> update
+
+    9. 회원 탈퇴  -> delete
+        - 1년간 보관? 완전 삭제?
+'''
+
+'''
+    파이썬에서 DB에 접속,접속 해제
 '''
 import pymysql as my
+
+connection = None
+try:
+    # 1.접속
+    connection = my.connect(host        ='localhost',  # 127.0.0.1 , 서버 주소
+                            #port        = 3306,        # 포트
+                            user        ='root',       # 사용자 계정, root 계정 외의 계정 사용 권장
+                            password    ='12341234',     # 비밀 번호
+                            database    = 'ml_db',        # 접속할 데이터베이스
+                            #cursorclass =my.cursors.DictCursor
+                            )
+    print('접속 성공')
+except Exception as e:
+    print('접속 오류',e)
+else:
+    print('접속시 문제없음')
+finally:
+    # 2. 접속 종료(I/o)->close()
+    if connection:
+        connection.close()
+    print('접속 종료 성공')
