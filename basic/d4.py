69e9d5cddcbc7387729f4a2820c4e14c5bf9f892
Highfloor <k01096789705@gmail.com>
2023-03-24 15:29:13 +0900

    feat: 로그인 함수(DB와 연계하여 쿼리를 수행하는)개발 완료

--- basic/d4.py
+++ basic/d4.py
@@ -3,7 +3,19 @@
 '''
 import pymysql as my
 def select_login(uid,upw):
+    '''
+        아이디, 비밀번호를 넣어서 회원여부를 체크하는 함수
+        parameter
+            - uid:아이디
+            - upw:비밀번호
+        returns
+            - 회원인 경우
+                - {'uid': 'guest', 'name': '게스트', 'regdate': datetime.datetime(2023, 3, 24, 13, 2, 47)}
+            - 비회원인경우 ,데이터베이스측 오류
+                - None
+    '''
     connection = None
+    row = None # 로그인 쿼리 수행 결과를 담는 변수
     try:
         connection = my.connect(host='localhost', #127.0.0.0 
                                 #port= 3306,
@@ -25,8 +37,8 @@ def select_login(uid,upw):
                     upw=%s;
                 '''
             cursor.execute(sql,(uid,upw))
-            row = cursor.fetchone()
-            print(row['name'])
+            row = cursor.fetchone()   # 결과셋중 한개만 가져온다 => 단수(리스트가 아닌 단독 타입:딕셔너리)
+            #print(row['name'])
             pass
     except Exception as e:
             print('접속 오류',e)
@@ -36,9 +48,13 @@ def select_login(uid,upw):
             # 2. 접속 종료(I/o)->close()
             if connection:
                 connection.close()
-            print('접속 종료 성공')
+    # 로그인한 결과를 리턴 -> {...}
+    return row
 
 if __name__ == '__main__':
     # d4 개발자의 테스트 코드
     # f5 개발자가 사용할때는 작동안함
-    select_login('guest','1234')
+    # 정상 계정
+    print(select_login('guest','1234'))
+    # 비정상 계정
+    print(select_login('guest','12345'))
